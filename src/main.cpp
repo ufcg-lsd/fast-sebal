3a5db631b43da875905361c025cb242bebdb31be
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-04-10 14:15:57 -0300

    Test 10, our output, becoming input...

--- src/main.cpp
+++ src/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, char *argv[]){
 
     ./run input/B2_converted.tif input/B3_converted.tif input/B4_converted.tif input/B5_converted.tif input/B6_converted.tif input/B7_converted.tif input/B10_converted.tif input/MTL.txt tal_converted.tif input/station.csv results -dist=0.98330
 
-*/
+
 int main(int argc, char *argv[]){
     string output_path = argv[11];
 
@@ -138,9 +138,9 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-/*
-./run input/MTL.txt input/station.csv -dist=0.98330
 
+./run input/MTL.txt input/station.csv -dist=0.98330
+*/
 void print_tiff(TIFF* tif) {
 
     uint32 heigth_band, width_band;
@@ -202,7 +202,7 @@ void fill_tiff(TIFF* tif, double min, double max){
     }
 }
 
-void setup(TIFF *tif) {
+/*void setup(TIFF *tif) {
 
     TIFFSetField(tif, TIFFTAG_IMAGEWIDTH     , 10);
     TIFFSetField(tif, TIFFTAG_IMAGELENGTH    , 10);
@@ -217,7 +217,7 @@ void setup(TIFF *tif) {
     TIFFSetField(tif, TIFFTAG_YRESOLUTION    , 1);
     TIFFSetField(tif, TIFFTAG_PLANARCONFIG   , PLANARCONFIG_CONTIG);
 
-}
+}*/
 
 int main(int argc, char *argv[]){
     
@@ -242,43 +242,43 @@ int main(int argc, char *argv[]){
     //setup(ndvi);
     //fill_tiff(ndvi, 0.13, 0.40);
     //TIFFClose(ndvi);
-    TIFF * ndvi = TIFFOpen("Testes/Teste06/meuNDVI.tif", "rm");
+    TIFF * ndvi = TIFFOpen("Testes/Teste04/NDVI.tif", "rm");
 
     //TIFF *surface_temperature = TIFFOpen("meuTS.tif", "w8m");
     //setup(surface_temperature);
     //fill_tiff(surface_temperature, 290, 315);
     //TIFFClose(surface_temperature);
-    TIFF *surface_temperature = TIFFOpen("Testes/Teste06/meuTS.tif", "rm");
+    TIFF *surface_temperature = TIFFOpen("Testes/Teste04/TS.tif", "rm");
 
     //TIFF *net_radiation = TIFFOpen("meuRn.tif", "w8m");
     //setup(net_radiation);
     //fill_tiff(net_radiation, 400, 655);
     //TIFFClose(net_radiation);
-    TIFF *net_radiation = TIFFOpen("Testes/Teste06/meuRn.tif", "rm");
+    TIFF *net_radiation = TIFFOpen("Testes/Teste04/Rn.tif", "rm");
 
     //TIFF *soil_heat = TIFFOpen("meuG.tif", "w8m");
     //setup(soil_heat);
     //fill_tiff(soil_heat, 70, 110);
     //TIFFClose(soil_heat);
-    TIFF *soil_heat = TIFFOpen("Testes/Teste06/meuG.tif", "rm");
+    TIFF *soil_heat = TIFFOpen("Testes/Teste04/G.tif", "rm");
 
     //TIFF *albedo = TIFFOpen("meuAlbedo.tif", "w8m");
     //setup(albedo);
     //fill_tiff(albedo, 0.15, 0.45);
     //TIFFClose(albedo);
-    TIFF *albedo = TIFFOpen("Testes/Teste06/meuAlbedo.tif", "rm");
+    TIFF *albedo = TIFFOpen("Testes/Teste04/alb.tif", "rm");
 
     Candidate hot_pixel = Candidate();
-    hot_pixel.col = 6;
-    hot_pixel.line = 4;
+    hot_pixel.col = 1439;
+    hot_pixel.line = 1407;
     hot_pixel.ndvi = read_position_tiff(ndvi, hot_pixel.col, hot_pixel.line);
     hot_pixel.soil_heat_flux = read_position_tiff(soil_heat, hot_pixel.col, hot_pixel.line);
     hot_pixel.temperature = read_position_tiff(surface_temperature, hot_pixel.col, hot_pixel.line);
     hot_pixel.net_radiation = read_position_tiff(net_radiation, hot_pixel.col, hot_pixel.line);
     
     Candidate cold_pixel = Candidate();
-    cold_pixel.col = 8;
-    cold_pixel.line = 2;
+    cold_pixel.col = 5764;
+    cold_pixel.line = 4288;
     cold_pixel.ndvi = read_position_tiff(ndvi, cold_pixel.col, cold_pixel.line);
     cold_pixel.soil_heat_flux = read_position_tiff(soil_heat, cold_pixel.col, cold_pixel.line);
     cold_pixel.temperature = read_position_tiff(surface_temperature, cold_pixel.col, cold_pixel.line);
@@ -329,13 +329,13 @@ int main(int argc, char *argv[]){
     //FIXME: auxiliar products TIFFs
     TIFF *zom, *ustar, *aerodynamic_resistence;
     zom = TIFFOpen("meuZom.tif", "w8m");
-    setup(zom);
+    setup(zom, ndvi);
 
     ustar = TIFFOpen("meuUstar.tif", "w8m");
-    setup(ustar);
+    setup(ustar, ndvi);
 
     aerodynamic_resistence = TIFFOpen("meuRah.tif", "w8m");
-    setup(aerodynamic_resistence);
+    setup(aerodynamic_resistence, ndvi);
 
     for(int line = 0; line < heigth_band; line++){
         read_line_tiff(ndvi, ndvi_line, line);
@@ -376,7 +376,7 @@ int main(int argc, char *argv[]){
 
     //It's only written into the rah cycle
     sensible_heat_flux = TIFFOpen("meuH.tif", "w8m");
-    setup(sensible_heat_flux);
+    setup(sensible_heat_flux, ndvi);
 
     int i = 0;
     bool Erro = true;
@@ -401,54 +401,54 @@ int main(int argc, char *argv[]){
             //Since ustar is both write and read into the rah cycle, two TIFF will be needed
             ustar_tif0 = TIFFOpen("meuUstar_2.tif", "rm");
             ustar_tif1 = TIFFOpen("meuUstar.tif", "w8m");
-            setup(ustar_tif1);
+            setup(ustar_tif1, ndvi);
             
             //Since ustar is both write and read into the rah cycle, two TIFF will be needed
             aerodynamic_resistence_tif0 = TIFFOpen("meuRah_2.tif", "rm");
             aerodynamic_resistence_tif1 = TIFFOpen("meuRah.tif", "w8m");
-            setup(aerodynamic_resistence_tif1);
+            setup(aerodynamic_resistence_tif1, ndvi);
             
         } else {
             //Since ustar is both write and read into the rah cycle, two TIFF will be needed
             ustar_tif0 = TIFFOpen("meuUstar.tif", "rm");
             ustar_tif1 = TIFFOpen("meuUstar_2.tif", "w8m");
-            setup(ustar_tif1);
+            setup(ustar_tif1, ndvi);
 
             //Since ustar is both write and read into the rah cycle, two TIFF will be needed
             aerodynamic_resistence_tif0 = TIFFOpen("meuRah.tif", "rm");
             aerodynamic_resistence_tif1 = TIFFOpen("meuRah_2.tif", "w8m");
-            setup(aerodynamic_resistence_tif1);
+            setup(aerodynamic_resistence_tif1, ndvi);
 
         }
 
         Ltif = TIFFOpen("meuL.tif", "w8m");
-        setup(Ltif);
+        setup(Ltif, ndvi);
 
         y01 = TIFFOpen("meuy01.tif", "w8m");
-        setup(y01);
+        setup(y01, ndvi);
 
         y2 = TIFFOpen("meuy2.tif", "w8m");
-        setup(y2);
+        setup(y2, ndvi);
 
         x200 = TIFFOpen("meux200.tif", "w8m");
-        setup(x200);
+        setup(x200, ndvi);
 
         psi01 = TIFFOpen("meupsi01.tif", "w8m");
-        setup(psi01);
+        setup(psi01, ndvi);
 
         psi2 = TIFFOpen("meupsi2.tif", "w8m");
-        setup(psi2);
+        setup(psi2, ndvi);
 
         psi200 = TIFFOpen("meupsi200.tif", "w8m");
-        setup(psi200);
+        setup(psi200, ndvi);
 
-        cout << "Ustar before loop" << endl;
-        print_tiff(ustar_tif0);
-        cout << endl;
+        //cout << "Ustar before loop" << endl;
+        //print_tiff(ustar_tif0);
+        // cout << endl;
 
-        cout << "Rah before loop" << endl;
-        print_tiff(aerodynamic_resistence_tif0);
-        cout << endl;
+        // cout << "Rah before loop" << endl;
+        // print_tiff(aerodynamic_resistence_tif0);
+        // cout << endl;
         
         for(int line = 0; line < heigth_band; line++){
             
@@ -505,49 +505,49 @@ int main(int argc, char *argv[]){
         TIFFClose(psi2);
         TIFFClose(psi200);
 
-        Ltif = TIFFOpen("meuL.tif", "rm");
-        y01 = TIFFOpen("meuy01.tif", "rm");
-        y2 = TIFFOpen("meuy2.tif", "rm");
-        x200 = TIFFOpen("meux200.tif", "rm");
-        psi01 = TIFFOpen("meupsi01.tif", "rm");
-        psi2 = TIFFOpen("meupsi2.tif", "rm");
-        psi200 = TIFFOpen("meupsi200.tif", "rm");
+        // Ltif = TIFFOpen("meuL.tif", "rm");
+        // y01 = TIFFOpen("meuy01.tif", "rm");
+        // y2 = TIFFOpen("meuy2.tif", "rm");
+        // x200 = TIFFOpen("meux200.tif", "rm");
+        // psi01 = TIFFOpen("meupsi01.tif", "rm");
+        // psi2 = TIFFOpen("meupsi2.tif", "rm");
+        // psi200 = TIFFOpen("meupsi200.tif", "rm");
 
-        cout << "L" << endl;
-        print_tiff(Ltif);
-        cout << endl;
+        // cout << "L" << endl;
+        // print_tiff(Ltif);
+        // cout << endl;
 
-        cout << "y_0.1" << endl;
-        print_tiff(y01);
-        cout << endl;
+        // cout << "y_0.1" << endl;
+        // print_tiff(y01);
+        // cout << endl;
 
-        cout << "y_2" << endl;
-        print_tiff(y2);
-        cout << endl;
+        // cout << "y_2" << endl;
+        // print_tiff(y2);
+        // cout << endl;
 
-        cout << "x_200" << endl;
-        print_tiff(x200);
-        cout << endl;
+        // cout << "x_200" << endl;
+        // print_tiff(x200);
+        // cout << endl;
 
-        cout << "psi_0.1" << endl;
-        print_tiff(psi01);
-        cout << endl;
+        // cout << "psi_0.1" << endl;
+        // print_tiff(psi01);
+        // cout << endl;
 
-        cout << "psi_2" << endl;
-        print_tiff(psi2);
-        cout << endl;
+        // cout << "psi_2" << endl;
+        // print_tiff(psi2);
+        // cout << endl;
 
-        cout << "psi_200" << endl;
-        print_tiff(psi200);
-        cout << endl;
+        // cout << "psi_200" << endl;
+        // print_tiff(psi200);
+        // cout << endl;
 
-        TIFFClose(Ltif);
-        TIFFClose(y01);
-        TIFFClose(y2);
-        TIFFClose(x200);
-        TIFFClose(psi01);
-        TIFFClose(psi2);
-        TIFFClose(psi200);
+        // TIFFClose(Ltif);
+        // TIFFClose(y01);
+        // TIFFClose(y2);
+        // TIFFClose(x200);
+        // TIFFClose(psi01);
+        // TIFFClose(psi2);
+        // TIFFClose(psi200);
 
         TIFFClose(ustar_tif0);
         TIFFClose(ustar_tif1);
@@ -607,7 +607,7 @@ int main(int argc, char *argv[]){
     TIFFClose(sensible_heat_flux);
     
     sensible_heat_flux = TIFFOpen("meuH.tif", "rm");
-    print_tiff(sensible_heat_flux);
+    //print_tiff(sensible_heat_flux);
 
     TIFF *latent_heat_flux, *net_radiation_24h, *latent_heat_flux_24h, *evapotranspiration_fraction, *evapotranspiration_24h;
 
@@ -656,4 +656,3 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
-*/
