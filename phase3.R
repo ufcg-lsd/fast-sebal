d7baed7b845d007555be49d085f92b4bc872cd11
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-04-12 09:01:40 -0300

    Phase 3...

--- phase3.R
+++ phase3.R
@@ -311,7 +311,7 @@ output.evapo<-stack(zom, ustar_final, rah, H_final, LE_final, Rn24h_dB_final, LE
 
 output.names <- c('zom', 'ustar_after', 'Rah_after', 'H', 'LatentHF', 'Rn24h', 'LatentHF24h', 'EF', 'ET24h')
 
-output.path <- "Testes/Teste11/R_OUTPUT"
+output.path <- "Testes/Teste11R/R_OUTPUT"
 
 names(output.evapo) <- output.names
 writeRaster(output.evapo, output.path, overwrite=TRUE, format="GTiff", varname="R_OUTPUT", varunit="daily", longname="R_OUTPUT", xname="lon", yname="lat", bylayer=TRUE, suffix="names")
