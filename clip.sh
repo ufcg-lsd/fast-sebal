124dabab27d1dc74b364987cca462a8881e4bdbd
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-05-03 08:33:43 -0300

    clip.sh ...

--- clip.sh
+++ clip.sh
@@ -6,10 +6,13 @@ END="_masked.nc"
 ENDT="_masked.tif"
 
 for i in $(ls $DIR | grep nc); do
-    FILE_NAME=$(echo $i | cut -d'.' -f1)
-    OUTPUT_FILE_NC="$FILE_NAME$END"
-    gdalwarp -cutline $MASK_PATH $i $OUTPUT_FILE_NC
-    OUTPUT_FILE_TIF="$FILE_NAME$ENDT"
-    gdal_translate -of GTiff $OUTPUT_FILE_NC $OUTPUT_FILE_TIF
+    echo "Working with file $i" &&
+    FILE_NAME=$(echo $i | cut -d'.' -f1) &&
+    OUTPUT_FILE_NC="$FILE_NAME$END" &&
+    echo "Staring cutline with mask $MASK_PATH from $i to $OUTPUT_FILE_NC" &&
+    gdalwarp -cutline $MASK_PATH $i $OUTPUT_FILE_NC &&
+    OUTPUT_FILE_TIF="$FILE_NAME$ENDT" &&
+    echo "Converting from $OUTPUT_FILE_NC to $OUTPUT_FILE_TIF" &&
+    gdal_translate -of GTiff $OUTPUT_FILE_NC $OUTPUT_FILE_TIF &&
     rm $OUTPUT_FILE_NC
 done
