2789861af331c4b39cb9bf0c224dfce356b4cdc3
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-05-03 08:21:06 -0300

    clip.sh ...

--- clip.sh
+++ clip.sh
@@ -2,11 +2,14 @@
 
 DIR=$1
 MASK_PATH=$2
-END="_masked.tif"
+END="_masked.nc"
+ENDT="_masked.tif"
 
-for i in $(ls $DIR | grep tif); do
-    OUTPUT_FILE="$(echo $i | cut -d'.' -f1)$END"
-    gdalwarp -cutline $MASK_PATH $i $OUTPUT_FILE
-    rm $i
-    mv $OUTPUT_FILE $i;
+for i in $(ls $DIR | grep nc); do
+    FILE_NAME=$(echo $i | cut -d'.' -f1)
+    OUTPUT_FILE_NC="$FILE_NAME$END"
+    gdalwarp -cutline $MASK_PATH $i $OUTPUT_FILE_NC
+    OUTPUT_FILE_TIF="$FILE_NAME$ENDT"
+    gdal_translate -of GTiff $OUTPUT_FILE_NC $OUTPUT_FILE_TIF
+    rm $OUTPUT_FILE_NC
 done
