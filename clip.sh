83de453cfefcd1ece25af945b9488272fc9e283a
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-05-03 09:33:11 -0300

    clip.sh ...

--- clip.sh
+++ clip.sh
@@ -9,14 +9,10 @@ for i in $(ls $DIR | grep nc); do
     FILE_NAME_FULL=$DIR$i
     FILE_NAME=$(echo $i | cut -d'.' -f1)
     OUTPUT_FILE_NC=$DIR$FILE_NAME$END
-    gdalwarp -cutline $MASK_PATH $FILE_NAME_FULL $OUTPUT_FILE_NC
+    gdalwarp -cutline -dstnodata 0 $MASK_PATH $FILE_NAME_FULL $OUTPUT_FILE_NC
     OUTPUT_FILE_TIF=$DIR$FILE_NAME$ENDT
     gdal_translate -of GTiff $OUTPUT_FILE_NC $OUTPUT_FILE_TIF
-    OUTPUT_FILE_TIF_AUX=$DIR$FILE_NAME"_nodata"$ENDT
-    gdalwarp -dstnodata 0 $OUTPUT_FILE_TIF $OUTPUT_FILE_TIF_AUX
     rm $OUTPUT_FILE_NC
-    rm $OUTPUT_FILE_TIF
-    mv $OUTPUT_FILE_TIF_AUX $OUTPUT_FILE_TIF
     OUTPUT_FILE_TIF_CONVERTED=$DIR$FILE_NAME"_converted.tif"
     ./test/conv $OUTPUT_FILE_TIF $OUTPUT_FILE_TIF_CONVERTED
     rm $OUTPUT_FILE_TIF
