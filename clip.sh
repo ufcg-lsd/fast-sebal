a5262dc4d2e0b7b8312741715e629b72edb1e733
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-05-03 11:26:53 -0300

    fix...

--- clip.sh
+++ clip.sh
@@ -2,18 +2,14 @@
 
 DIR=$1
 MASK_PATH=$2
-END="_masked.nc"
-ENDT="_masked.tif"
+END="_masked.tif"
 
 for i in $(ls $DIR | grep nc); do
     FILE_NAME_FULL=$DIR$i
     FILE_NAME=$(echo $i | cut -d'.' -f1)
-    OUTPUT_FILE_NC=$DIR$FILE_NAME$END
-    gdalwarp -dstnodata 0 -cutline $MASK_PATH $FILE_NAME_FULL $OUTPUT_FILE_NC
-    OUTPUT_FILE_TIF=$DIR$FILE_NAME$ENDT
-    gdal_translate -of GTiff $OUTPUT_FILE_NC $OUTPUT_FILE_TIF
-    #rm $OUTPUT_FILE_NC
-    OUTPUT_FILE_TIF_CONVERTED=$DIR$FILE_NAME"_converted.tif"
-    ./test/conv $OUTPUT_FILE_TIF $OUTPUT_FILE_TIF_CONVERTED
+    OUTPUT_FILE=$DIR$FILE_NAME$END
+    gdalwarp -q -cutline $MASK_PATH -tr 0.000272 0.000271 -of GTiff $FILE_NAME_FULL $OUTPUT_FILE
+    OUTPUT_FILE_CONVERTED=$DIR$FILE_NAME"_converted.tif"
+    ./test/conv $OUTPUT_FILE $OUTPUT_FILE_CONVERTED
     #rm $OUTPUT_FILE_TIF
 done
