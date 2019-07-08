f0aa4b32bff94eea09f7f002e0a489b2acf3e671
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-07-08 20:47:56 +0000

    getting data...

--- execute.sh
+++ execute.sh
@@ -3,7 +3,8 @@
 make clean
 make
 ./run input/_B2_converted.tif input/_B3_converted.tif input/_B4_converted.tif input/_B5_converted.tif input/_B6_converted.tif input/_B7_converted.tif input/_B10_converted.tif input/MTL.txt tal_converted.tif input/station.csv Testes/TesteClean/ -dist=0.98330 > Testes/TesteClean/outC &
-PID=$$
-sh collect-cpu-usage.sh $PID > /home/ubuntu/TDir/cpu.csv &
-sh collect-memory-usage.sh $PID > /home/ubuntu/TDir/mem.csv &
-sh collect-disk-usage.sh $PID > /home/ubuntu/TDir/disk.csv
+PID=$(pidof ./run)
+ps -aux | grep $PID
+sh scripts/collect-cpu-usage.sh $PID > cpu.csv &
+sh scripts/collect-memory-usage.sh $PID > mem.csv &
+sh scripts/collect-disk-usage.sh $PID > disk.csv
