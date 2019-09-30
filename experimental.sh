bb01594ae5c274437f5f63c47772aaa4042c22c8
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-09-30 15:53:18 -0300

    experiment

--- experimental.sh
+++ experimental.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-make
 for i in $(seq 1 30)
 do
 	echo Executing ${i}th experiment
 	OUTPUTPATH=DataExecution/execution${i}
 	mkdir $OUTPUTPATH
 	echo Output repository created and starting execution
-	bash ./execute.sh $OUTPUTPATH &&
-	echo Execution finished
-	echo 'Tarzing TIFs'
-	tar -cvzf $OUTPUT_PATH/execution${i}.tar.gz $OUTPUT_PATH/*.tif
-	rm $OUTPUT_PATH/*.tif
-	rm DataExecution/*.tif
+	time ./execute.sh $OUTPUTPATH &&
 	echo Completing ${i}th experiment
 done
 cd ..
-make clean
