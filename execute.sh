d28e5a1fc2a83c8a14903e6535c1ac9ebda18b44
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-09-30 15:29:41 -0300

    enviroment to experiment

--- execute.sh
+++ execute.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+OUTPUT_PATH=$1
+
 make clean
 make
-./run input/_B2_converted.tif input/_B3_converted.tif input/_B4_converted.tif input/_B5_converted.tif input/_B6_converted.tif input/_B7_converted.tif input/_B10_converted.tif input/MTL.txt tal_converted.tif input/station.csv Testes/TesteClean/ -dist=0.98330 > Testes/TesteClean/outC &
+./run input/_B2_converted.tif input/_B3_converted.tif input/_B4_converted.tif input/_B5_converted.tif input/_B6_converted.tif input/_B7_converted.tif input/_B10_converted.tif input/MTL.txt input/tal_converted.tif input/station.csv $OUTPUT_PATH -dist=0.98330 > $OUTPUT_PATH/out.txt &
 PID=$(pidof ./run)
 ps -aux | grep $PID
-sh scripts/collect-cpu-usage.sh $PID > cpu.csv &
-sh scripts/collect-memory-usage.sh $PID > mem.csv &
-sh scripts/collect-disk-usage.sh $PID > disk.csv
+sh scripts/collect-cpu-usage.sh $PID > $OUTPUT_PATH/cpu.csv &
+sh scripts/collect-memory-usage.sh $PID > $OUTPUT_PATH/mem.csv &
+sh scripts/collect-disk-usage.sh $PID > $OUTPUT_PATH/disk.csv
