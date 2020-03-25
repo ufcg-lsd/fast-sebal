96eeea9044e33b4856e92813f60286c6c6795e2e
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2020-03-25 11:01:11 -0300

    adding debug print

--- src/products.cpp
+++ src/products.cpp
@@ -12,6 +12,8 @@
 void radiance_function(TIFF* read_bands[], MTL mtl, Sensor sensor, int width_band, int line, double radiance_line[][8], double noData){
     double line_band[width_band];
 
+    int cont = 0;
+
     if (mtl.number_sensor == 8){
         read_line_tiff(read_bands[7], line_band, line);
         for (int col = 0; col < width_band; col++) {
@@ -21,8 +23,11 @@ void radiance_function(TIFF* read_bands[], MTL mtl, Sensor sensor, int width_ban
     else{
         for (int i = 1; i < 8; i++){
             read_line_tiff(read_bands[i], line_band, line);
-            for (int col = 0; col < width_band; col++)
+            for (int col = 0; col < width_band; col++) {
                 radiance_line[col][i] = min(line_band[col] != noData ? line_band[col] * sensor.parameters[i][sensor.GRESCALE] + sensor.parameters[i][sensor.BRESCALE] : NaN, 0.0);
+                if (radiance_line[col][i] == NaN) cont++;
+            }
+            cout << "Band" << i << ", Line: " << line << ", NaN: " << cont << endl;
         }
     }
 
